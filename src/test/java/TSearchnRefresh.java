975bd01bce5d8feeb914d3e3a87c8ff36efe7bf1
OLAPTEV <o.laptev@xceptance.net>
2023-04-03 12:55:08 +0200

    Added new test scenario

---
+++ src/test/java/TSearchnRefresh.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class TSearchnRefresh {
+
+    public void openAndRefresh(){
+        File file = new File("src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.navigate().to("https://www.youtube.com/");
+        driver.navigate().refresh();
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
