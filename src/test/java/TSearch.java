4868b3aa5402ec9da07321eb9254c03616fd949b
OLAPTEV <o.laptev@xceptance.net>
2023-04-03 12:50:04 +0200

    Fixed typos and allowed external connections

--- src/test/java/TSearch.java
+++ src/test/java/TSearch.java
@@ -1,16 +1,20 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+    import org.testng.internal.Configuration;
 
 import java.io.File;
 
 public class TSearch {
     @Test
-    public void openGoogleComInChromeTest(){
+    public void openWebDEinChromeTest(){
         File file = new File("src/test/resources/chromedriver.exe");
-        System.setProperty("webdriver.com.driver", file.getAbsolutePath());
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://www.google.com/");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.navigate().to("https://web.de/");
         System.out.println(driver.getTitle());
         driver.quit();
     }
