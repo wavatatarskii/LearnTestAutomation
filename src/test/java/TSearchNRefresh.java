3bf59eaf24eb87790c92a4af8a0b3b8ec56166ce
OLAPTEV <o.laptev@xceptance.net>
2023-04-12 16:44:32 +0200

    Added new functionality, restructured class delegating specific actions to helper methods

--- src/test/java/TSearchNRefresh.java
+++ src/test/java/TSearchNRefresh.java
@@ -1,21 +1,31 @@
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.List;
 
 public class TSearchNRefresh {
 
     @Test
-    public void openAndRefresh(){
+    public void TScenario(){
         File file = new File("src/test/resources/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         ChromeDriver driver = new ChromeDriver(options);
-        driver.navigate().to("https://www.youtube.com/");
-        driver.navigate().refresh();
-        System.out.println(driver.getTitle());
+        openAndRefresh(driver);
+        clickOnSearch(driver);
         driver.quit();
+    }
+    public void openAndRefresh(ChromeDriver driver){
+        driver.navigate().to("https://www.google.com/");
+        driver.navigate().refresh();
+        System.out.println(driver.getTitle()+ " has been opened");
+    }
+    public void clickOnSearch(ChromeDriver driver){
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.click();
     }
 }
